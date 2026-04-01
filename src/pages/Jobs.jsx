2ca433b863090e2f875eebf1ca20f3d030d63c33
Roberto Ortiz <roberto.mendez0332@alumnos.udg.mx>
2026-04-01 17:25:09 -0600

    Fix missing Link import in Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Jobs.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
